f529a5bed282a3219e4a2e94292b584351485904
Thanh Tam <thanhtam.v2t@gmail.com>
2021-05-24 15:00:20 +0700

    Upload

---
+++ tendermint/index.js
@@ -0,0 +1,12 @@
+import { PrivKeyEd25519, PubKeyEd25519 } from "./types/ed25519";
+import { PrivKeySecp256k1, PubKeySecp256k1 } from "./types/secp256k1";
+import { codec } from "../codec";
+// export * as tendermint from "./module";
+import * as tendermint from "./module";
+export { tendermint };
+export * from "./types";
+// Register codec
+codec.registerCodec("tendermint/PrivKeyEd25519", PrivKeyEd25519, PrivKeyEd25519.fromJSON);
+codec.registerCodec("tendermint/PubKeyEd25519", PubKeyEd25519, PubKeyEd25519.fromJSON);
+codec.registerCodec("tendermint/PrivKeySecp256k1", PrivKeySecp256k1, PrivKeySecp256k1.fromJSON);
+codec.registerCodec("tendermint/PubKeySecp256k1", PubKeySecp256k1, PubKeySecp256k1.fromJSON);
